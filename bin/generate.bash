a3607a01b2f588cafbe5b0b8dbbadac4c12842f3
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-10-02 12:01:29 +0900

    Add chime.wav

--- bin/generate.bash
+++ bin/generate.bash
@@ -5,6 +5,7 @@ set -e
 [[ ! -d ./wav ]] && mkdir ./wav
 
 # Generate sound effects.
+sox -v 1.375 -r 22050 -c 1 ./src/chime.aif -b 16 ./wav/chime.wav speed 4.5 trim 16.25 0.75 fade h 0 0.69 0.69
 sox -v 1.75 -r 22050 -c 1 ./src/chop.wav ./wav/move.wav speed 3.0 trim 3.39 0.05 pitch 1080 lowpass 1600 fade t 0 0.08 0.08
 sox -v 2.0 -r 22050 -c 1 ./src/bubble.wav ./wav/oops.wav speed 16.0 fade t 0 0.075 0.075
 sox -v 1.5 -r 22050 ./src/castanets.wav -b 16 ./wav/password.wav trim 0.005 0.375 speed 4.75 pitch "-240" fade t 0 0.05 0.05
